3ba122b6947c5ff56a7ead7b91ffe4c430ae5f3f
hashik <mohamedhashik.kp@gmail.com>
2020-09-16 21:34:16 +0530

    Updates

---
+++ precache-manifest.1a0f5ff5df7c3f2da23cc092464013ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c633bb1e97538bd2b864510bbaa7e820",
+    "url": "/covid19-reactapp/index.html"
+  },
+  {
+    "revision": "b5ab236e0c928a8b3598",
+    "url": "/covid19-reactapp/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c7142801dc01ea65f99f",
+    "url": "/covid19-reactapp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b5ab236e0c928a8b3598",
+    "url": "/covid19-reactapp/static/js/2.d23c6a9c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/covid19-reactapp/static/js/2.d23c6a9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7142801dc01ea65f99f",
+    "url": "/covid19-reactapp/static/js/main.5b4e5c81.chunk.js"
+  },
+  {
+    "revision": "8e601bd329b62e47ac25",
+    "url": "/covid19-reactapp/static/js/runtime-main.959f1980.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.30479e5a7c7bac9c1b27ce56938b1fba.js"
+  "/covid19-reactapp/precache-manifest.1a0f5ff5df7c3f2da23cc092464013ed.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/covid19-reactapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
